13802805b0c0c7a7a7dfdcb118c7678cca91511a
pavankumarsimma <pavankumarsimma08@gmail.com>
2024-01-26 22:55:10 +0530

    second commit

---
+++ client/mailclient.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define MAXSIZE 1000
+int main(){
+    struct sockaddr_in serv_addr;
+
+    int cli_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (cli_sock == -1){
+        perror("Socket creation Failed");
+        exit(EXIT_FAILURE);
+    }
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(3400);
+    int x = connect(cli_sock, (const struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (x == -1){
+        perror("Connect error");
+        exit(EXIT_FAILURE);
+    }
+    char buffer[MAXSIZE];
+    printf("Connected to server\n");
+    recv(cli_sock, buffer, MAXSIZE, 0);
+    printf("[+] %s\n", buffer);
+    return 0;
+}
---
+++ server/smtpmail.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char* argv[]){
+	int my_port = 0;
+	if (argc==1) {
+		printf("Usage: ./a.out <PORT>");
+		exit(0);
+	}
+	my_port = atoi(argv[1]);
+	
+	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == -1) {
+		perror("Socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	struct sockaddr_in serv_addr, cli_addr;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(my_port);
+	// bind 
+	if ( bind(server_socket, (const struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+		perror("Binding error");
+		exit(EXIT_FAILURE);
+	}  
+	
+	// server will listen for the incoming connetions
+	int x = listen(server_socket, 5);
+	if (x==-1) {
+		perror("Listen Error");
+		exit(EXIT_FAILURE);
+	}
+	printf("Server listening ...\n");
+	while (1) {
+		int len = sizeof(cli_addr);
+		int new_socket = accept(server_socket, (struct sockaddr*)&cli_addr, &len);
+		if (new_socket == -1) {
+			perror("accept error");
+			exit(EXIT_FAILURE);
+		}
+		// fork a child process
+		int pid = fork();
+		if (pid == 0){
+			// child
+			close(server_socket);
+			// handle the client
+			
+			
+			close(new_socket);
+			exit(EXIT_SUCCESS);
+		}
+		close(new_socket);
+		while(wait(NULL)>0) ;
+	}
+	return 0;
+}
+
