3f9fcc4ef94cf1f77f661db53565ed9cdd753928
pavankumarsimma <pavankumarsimma08@gmail.com>
2024-02-01 00:01:40 +0530

    14 commit

--- client/c.c
+++ client/c.c
@@ -8,7 +8,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #define MAXSIZE 100
-
+#define LINESIZE 80
 char *getLocalIP();
 int main(int argc, char* argv[]){
     struct sockaddr_in serv_addr;
@@ -19,79 +19,197 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
 
-    int cli_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (cli_sock == -1){
-        perror("Socket creation Failed");
-        exit(EXIT_FAILURE);
-    }
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(atoi(argv[2]));
-    int x = connect(cli_sock, (const struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    if (x == -1){
-        perror("Connect error");
-        exit(EXIT_FAILURE);
-    }
-    char buffer[MAXSIZE];
-    printf("Connected to SMTP server\n");
-
+    int option = 0;
     int flag = 1;
-    int notdata = 1;
     while(flag == 1){
-
-        // receive response from server
-        memset(buffer, '\0', MAXSIZE);
-        if (notdata == 1){
-            n = recv(cli_sock, buffer, MAXSIZE, 0);
-            if (n < 0) {
-                perror("recv error");
+        printf("Menu: \n");
+        printf("1. Manage Mail : Shows the stored mails of the logged in user only\n");
+        printf("2. Send Mail : Allows the user to send a mail\n");
+        printf("3. Quit : Quits the program.\n");
+        printf("Select : ");
+        scanf("%d", &option);
+        if (option == 3){
+            printf("Quitting\n");
+            flag=0;
+        }
+        else if ( option==2){
+            // smtp part
+            int cli_sock = socket(AF_INET, SOCK_STREAM, 0);
+            if (cli_sock == -1){
+                perror("Socket creation Failed");
                 exit(EXIT_FAILURE);
             }
-            printf("S: %s\n", buffer);
-        }
-        
+            serv_addr.sin_family = AF_INET;
+            serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+            serv_addr.sin_port = htons(atoi(argv[2]));
+            int x = connect(cli_sock, (const struct sockaddr*)&serv_addr, sizeof(serv_addr));
+            if (x == -1){
+                perror("Connect error");
+                exit(EXIT_FAILURE);
+            }
+            char buffer[MAXSIZE];
+            printf("Connected to SMTP server\n");
+            int n;
 
-        char code[3];
-        code[0] = buffer[0];
-        code[1] = buffer[1];
-        code[2] = buffer[2];
-
-        if(strncmp(code, "221", 3)==0){
-            // closing the connection
-            flag = 0;
-            notdata = 1;
-            continue;
-        }
-        else if(strncmp(code, "354", 3)==0 ){
-            printf("Enter the mail data in correct format\n");
-            notdata = 0;
+            printf("Enter the mail in correct format\n");
+            char mail[53][LINESIZE];
+            for(int i=0; i<53; i++){
+                memset(mail[i], '\0', LINESIZE);
+            }
+            getchar();
+            int mail_endflag = 1;
+            for(int i=0; i<53; i++){
+                memset(mail[i], '\0', LINESIZE);
+                fgets(mail[i], LINESIZE, stdin);
+                // printf("Entered: %s", mail[i]);
+                if ( strncmp(mail[i], ".", 1)==0 ){
+                    break;
+                }
+                if ( i==52 ){
+                    mail_endflag = 0;
+                }
+            }
+            // format checking
+            char from[MAXSIZE];
+            char to[MAXSIZE];
+            char subject[51];
             
-        }
-        else if(strncmp(code, "250", 3) == 0){
-            notdata = 1;
-        }
-        else if(strncmp(code, "600", 3)==0){
-            notdata = 1;
-        }
+            if (sscanf(mail[0], "From: %[^\n]", from) != 1) {
+                printf("%s\n", from);
+                printf("Incorrect 'From' format\n");
+                close(cli_sock);
+                continue;
+            }
+            int c=0;
+            if (from[0] == '@'){
+                printf("%s\n", from);
+                printf("Incorrect 'From' format\n");
+                close(cli_sock);
+                continue;
+            }
+            for(int j=0; j<strlen(from); j++){
+                if (from[j] == '@'){
+                    if (c==0){
+                        c = 1;
+                    }
+                    else {
+                        c = 0;
+                        break;
+                    }
+                }
+            }
+            if (c!=1){
+                printf("%s\n", from);
+                printf("Incorrect 'From' format\n");
+                close(cli_sock);
+                continue;
+            }
+            if (sscanf(mail[1], "To: %[^\n]", to) != 1) {
+                printf("%s\n", to);
+                printf("Incorrect 'To' format\n");
+                close(cli_sock);
+                continue;
+            }
+            c=0;
+            if (to[0] == '@'){
+                printf("%s\n", to);
+                printf("Incorrect 'To' format\n");
+                close(cli_sock);
+                continue;
+            }
+            for(int j=0; j<strlen(to); j++){
+                if (to[j] == '@'){
+                    if (c==0){
+                        c = 1;
+                    }
+                    else {
+                        c = 0;
+                        break;
+                    }
+                }
+            }
+            if (c!=1){
+                printf("%s\n", to);
+                printf("Incorrect 'To' format\n");
+                close(cli_sock);
+                continue;
+            }
+            if (sscanf(mail[2], "Subject: %50[^\n]", subject) != 1) {
+                printf("%s\n", subject);
+                printf("Incorrect 'Subject' format\n");
+                close(cli_sock);
+                continue;
+            }
+            if (mail_endflag == 0){
+                printf("No <CRLF>.<CRLF> end to mail body\n");
+                close(cli_sock);
+                continue;
+            }
+
+            // for(int i=0; i<53; i++){
+            //     printf("%s", mail[i]);
+            // }
+            printf("Mail ok\n");
 
 
-        // send request to server
-        memset(buffer, '\0', MAXSIZE);
-        printf("Enter(80 chars max) >  ");
-        fgets(buffer, MAXSIZE, stdin);
-        if (strncmp(buffer, ".", 1) == 0) {
-            notdata = 1;
+            // mail session
+            memset(buffer, '\0', MAXSIZE);
+            n = recv(cli_sock, buffer, MAXSIZE, 0);
+            if (n<0){
+                perror("recv error");
+                close(cli_sock);
+                break;
+            }
+            if (n==0) {
+                printf("server disconnected\n");
+                close(cli_sock);
+                break;
+            }
+            printf("S: %s\n", buffer);
+
+            // HAVE TO IMPLEMENT
+
+            memset(buffer, '\0', MAXSIZE);
+            sprintf(buffer, "QUIT\r\n");
+            n = send(cli_sock, buffer, strlen(buffer), 0);
+            if (n<0){
+                perror("send error");
+                close(cli_sock);
+                break;
+            }
+            if (n==0) {
+                printf("server disconnected\n");
+                close(cli_sock);
+                break;
+            }
+            printf("C: %s\n", buffer);
+
+            memset(buffer, '\0', MAXSIZE);
+            n = recv(cli_sock, buffer, MAXSIZE, 0);
+            if (n<0){
+                perror("recv error");
+                close(cli_sock);
+                break;
+            }
+            if (n==0) {
+                printf("server disconnected\n");
+                close(cli_sock);
+                break;
+            }
+            printf("S: %s\n", buffer);
+
+            close(cli_sock);
         }
-        sprintf(buffer, "%s\r\n", buffer);
-        n = send(cli_sock, buffer, strlen(buffer), 0);
-        if(n<0) {
-            perror("send error");
-            exit(EXIT_FAILURE);
+        else if (option == 1){
+            printf("POP3 part not yet implemented\n");
         }
-        printf("C: %s\n", buffer);
+        else {
+            printf("Invalid option\n");
+            printf("Quitting\n");
+            flag=0;
+        }
     }
 
-    close(cli_sock);
     return 0;
 }
 
